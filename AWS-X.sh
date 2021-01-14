2a84c03716035882b24e5d25cfe0d8d72dbcd8a9
rafifauz <rafitugas3@gmail.com>
2021-01-14 16:12:26 +0700

    IP Upload

--- AWS-X.sh
+++ AWS-X.sh
@@ -1,3 +1,4 @@
+cd /home/raxer/Raxer/Cilsy/AWS/AWS_Loadbalancer && cat " "> ip.txt
 echo "---------Create Instance Mysql Database------------"
 aws ec2 run-instances --image-id ami-06fb5332e8e3e577a --count 1 --instance-type t2.micro --key-name CilsyAWS --security-group-ids sg-0cecc5906da0ce444 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Nginx_DB}]' 'ResourceType=volume,Tags=[{Key=Name,Value=Nginx_DB}]' --user-data '#!/bin/bash
 sudo apt-get install expect -y
@@ -8,8 +9,6 @@ sudo chmod +x setting_Mysql.sh'
 
 echo "-------------------upload to git------------------"
 cd /home/raxer/Raxer/Cilsy/AWS/AWS_Loadbalancer && git add -A && git commit -m "IP Upload" && git push origin main
-
-
 
 IP1=$(aws ec2 describe-instances | grep -w -m 1 "PrivateIpAddress" | tr -d '"'','| awk '{print $2}' | sort -u)
 echo "---------Create Instance Nginx Webserver 1------------"
